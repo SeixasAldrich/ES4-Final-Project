8b19ee2d5fe3b002368e5237f84633fac1bebae0
Khaled-Ajaj <83191304+Khaled-Ajaj@users.noreply.github.com>
2021-04-26 01:11:25 -0400

    Update progrom.vhd

--- progrom.vhd
+++ progrom.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use std.textio.all;
 
 entity progrom is
 
@@ -11,10 +12,27 @@ entity progrom is
 
 end;
 
+
+
 architecture synth of progrom is
 
+constant rom_depth : natural := 2**10;
+constant rom_width : natural := 32;
+
+ 
+type rom_type is array (0 to rom_depth - 1)
+  of std_logic_vector(rom_width - 1 downto 0);
+  
+ signal rom: rom_type;
 begin
+	--hard coding memeory
+	process (all) begin
+		for i in 0 to (rom_depth-1) loop
+			rom(i) <= std_logic_vector(to_unsigned(2*i, rom_width));
+		end loop;
+		
+	end process;
 
-
+data <= rom(to_integer(addr));
 
 end;
