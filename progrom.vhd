dde1bb857abc0c5ae82984edd30aaa339555edd6
alan-cepeda <79609530+alan-cepeda@users.noreply.github.com>
2021-04-25 22:47:00 -0400

    Create progrom.vhd

---
+++ progrom.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity progrom is
+
+	port(
+		addr : in unsigned(31 downto 0);
+		data : out std_logic_vector(31 downto 0)
+		);
+
+end;
+
+architecture synth of progrom is
+
+signal branch : std_logic;
+signal clk : std_logic;
+signal reset : std_logic;
+
+begin
+
+
+
+end;
