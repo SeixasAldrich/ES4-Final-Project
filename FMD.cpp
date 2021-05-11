bba9c534c2a72cc39fdab04c0d9617bf016e34b7
SeixasAldrich <78521803+SeixasAldrich@users.noreply.github.com>
2021-05-11 18:33:04 -0400

    Add files via upload

---
+++ FMD.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    vector<string> temp_values;
+    vector<string> output_values;
+    
+    ifstream infile(argv[1]);
+    
+    if (infile.fail()) {
+        cerr << "ERROR: The file " << argv[1] << " failed to open correctly." << endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    string temp = "";
+    
+    while (!infile.eof()) {
+        
+        infile >> temp;
+        temp_values.push_back(temp);
+        infile.ignore(256, '\n');
+        
+    }
+    
+    for(int i = 0; i < temp_values.size(); i++) {
+        output_values.push_back("when 32d\"" + i + "\" => data <= \"" + temp_values[i] + "\";");
+    }
+    
+    for(int i = 0; i < temp_values.size(); i++) {
+        cout << temp_values[i];
+        cout << endl;
+    }
+    
+    return 0;
+}
